395ec4e504e83214c4828aeddf10a06e2285cfd1
franktang <hktxw0103@gmail.com>
2018-04-12 17:32:04 +0800

    remove unsed method and correct test cases

--- src/PokerCardHand.py
+++ src/PokerCardHand.py
@@ -17,31 +17,12 @@ class PokerCardHand:
             output_string += card.get_card_in_string()
         return output_string
 
-    def is_high_card(self):
-        for card in self.cards:
-            if self.get_count_of_same_value(card.value) > 1:
-                return False
-        return True
-
-    def get_count_of_same_value(self, search_value):
-        count = 0
-        for card in self.cards:
-            if search_value == card.value:
-                count += 1
-        return count
 
     @staticmethod
     def get_count_of_same_value_static(cards, search_value):
         count = 0
         for card in cards:
             if search_value == card.value:
-                count += 1
-        return count
-
-    def get_count_of_same_suit(self, search_value):
-        count = 0
-        for card in self.cards:
-            if search_value == card.suit:
                 count += 1
         return count
 
@@ -53,46 +34,12 @@ class PokerCardHand:
                 count += 1
         return count
 
-    def get_count_of_same_value_or_suit(self, value_or_suit, search_value):
-        count = 0
-        for card in self.cards:
-            if search_value == card.value:
-                count += 1
-        return count
-
-
-    def is_pair(self):
-        for card in self.cards:
-            if self.get_count_of_same_value(card.value) == 2:
-                return True
-
-        return False
-
-    def is_straight(self):
-        sorted_card_list = sorted(self.cards, key=lambda card: card.rank)
-        return self.is_continous(sorted_card_list) or (self.is_include_straight_pattern(sorted_card_list))
-
-    def is_continous(self, sorted_card_list):
-        for index in range(1, len(sorted_card_list)):
-            if sorted_card_list[index].rank - sorted_card_list[index-1].rank != 1:
-                return False
-        return True
-
     @staticmethod
     def is_continous_static(sorted_card_list):
         for index in range(1, len(sorted_card_list)):
             if sorted_card_list[index].rank - sorted_card_list[index-1].rank != 1:
                 return False
         return True
-
-    def is_include_straight_pattern(self, sorted_card_list):
-        cards_value_string = ""
-        for card in sorted_card_list:
-            cards_value_string += card.value
-
-        if cards_value_string in self.include_straight_pattern:
-            return True
-        return False
 
     @staticmethod
     def is_include_straight_pattern_static(sorted_card_list):
@@ -104,24 +51,6 @@ class PokerCardHand:
         if cards_value_string in include_straight_pattern:
             return True
         return False
-
-    def is_three_of_a_kind(self):
-        for card in self.cards:
-            if self.get_count_of_same_value(card.value) == 3:
-                return True
-        return False
-
-    def is_flush(self):
-        for card in self.cards:
-            if self.get_count_of_same_suit(card.suit) == 5:
-                return True
-        return False
-
-    def is_full_house(self):
-        return self.is_pair() and self.is_three_of_a_kind()
-
-    def is_royal_flush(self):
-        return self.is_straight() and self.is_flush()
 
     def determine_category(self):
         strategy_list = [
@@ -138,20 +67,6 @@ class PokerCardHand:
             if strategy.is_obey_this_strategy(self.cards):
                 return strategy.category_name
 
-        # if self.is_royal_flush():
-        #     return "ROYAL_FLUSH"
-        # elif self.is_full_house():
-        #     return "FULL_HOUSE"
-        # elif FlushStrategy().is_obey_this_strategy(self.cards):
-        #     return FlushStrategy().category_name
-        # elif StraightStrategy().is_obey_this_strategy(self.cards):
-        #     return StraightStrategy().category_name
-        # elif self.is_three_of_a_kind():
-        #     return "THREE_OF_A_KIND"
-        # elif self.is_pair():
-        #     return "PAIR"
-        # else:
-        #     return "HIGH_CARD"
 
 
 class CategoryStrategy:
--- test/TestPoker.py
+++ test/TestPoker.py
@@ -2,6 +2,7 @@ import unittest
 
 from PokerCardHand import PokerCardHand
 from PokerCard import PokerCard
+from PokerCardHand import *
 
 
 class TestPoker(unittest.TestCase):
@@ -23,81 +24,81 @@ class TestPoker(unittest.TestCase):
         card_hand_input = ("AS", "3D", "6C", "JS", "QC")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_high_card()
+        result = card_hand.determine_category()
 
-        self.assertTrue(result)
+        self.assertEqual(result, HighCardStrategy.category_name)
 
     def test_given_one_pair_card_hand_input_when_check_is_high_card_should_return_false(self):
         card_hand_input = ("AS", "3D", "3C", "JS", "QC")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_high_card()
+        result = card_hand.determine_category()
 
-        self.assertFalse(result)
+        self.assertNotEqual(HighCardStrategy.category_name, result)
 
     def test_given_one_pair_card_hand_input_when_check_is_pair_should_return_true(self):
         card_hand_input = ("AS", "3D", "3C", "JS", "QC")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_pair()
+        result = card_hand.determine_category()
 
-        self.assertTrue(result)
+        self.assertEqual(PairStrategy.category_name, result)
 
     def test_given_three_of_a_kind_card_hand_input_when_check_is_pair_should_return_false(self):
         card_hand_input = ("AS", "3D", "3C", "3S", "QC")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_pair()
+        result = card_hand.determine_category()
 
-        self.assertFalse(result)
+        self.assertNotEqual(PairStrategy.category_name, result)
 
     def test_given_normal_straight_card_hand_input_when_check_is_straight_should_return_true(self):
         card_hand_input = ("3S", "4D", "5C", "6S", "7C")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_straight()
+        result = card_hand.determine_category()
 
-        self.assertTrue(result)
+        self.assertEqual(StraightStrategy.category_name, result)
 
     def test_given_A2345_straight_card_hand_input_when_check_is_straight_should_return_true(self):
         card_hand_input = ("AS", "2D", "3C", "4S", "5C")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_straight()
+        result = card_hand.determine_category()
 
-        self.assertTrue(result)
+        self.assertEqual(StraightStrategy.category_name, result)
 
     def test_given_23456_straight_card_hand_input_when_check_is_straight_should_return_true(self):
         card_hand_input = ("6S", "2D", "3C", "4S", "5C")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_straight()
+        result = card_hand.determine_category()
 
-        self.assertTrue(result)
-
-    def test_given_10JQKA_straight_card_hand_input_when_check_is_straight_should_return_true(self):
-        card_hand_input = ("10S", "JD", "QC", "KS", "AC")
-        card_hand = PokerCardHand(card_hand_input)
-
-        result = card_hand.is_straight()
-
-        self.assertTrue(result)
+        self.assertEqual(StraightStrategy.category_name ,result)
 
     def test_given_10JQKA_straight_card_hand_input_when_check_is_straight_should_return_true(self):
         card_hand_input = ("10S", "JD", "QC", "KS", "AC")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_straight()
+        result = card_hand.determine_category()
 
-        self.assertTrue(result)
+        self.assertEqual(StraightStrategy.category_name, result)
+
+    def test_given_10JQKA_straight_card_hand_input_when_check_is_straight_should_return_true(self):
+        card_hand_input = ("10S", "JD", "QC", "KS", "AC")
+        card_hand = PokerCardHand(card_hand_input)
+
+        result = card_hand.determine_category()
+
+        self.assertEqual(StraightStrategy.category_name, result)
 
     def test_given_three_of_a_kind_card_hand_input_when_check_is_three_of_a_kind_should_return_true(self):
         card_hand_input = ("AS", "3D", "3C", "3S", "QC")
         card_hand = PokerCardHand(card_hand_input)
 
-        result = card_hand.is_three_of_a_kind()
+        result = card_hand.determine_category()
 
-        self.assertTrue(result)
+        self.assertEqual(ThreeOfAKindStrategy.category_name, result)
 
     def test_given_flush_card_hand_input_when_check_is_flush_should_return_true(self):
         card_hand_input = ("AS", "5S", "3S", "9S", "JS")
